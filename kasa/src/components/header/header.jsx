cbc5c3a65ba2f9a39f4080bccca053418d77f00c
Adele M <moutoussamy.adele@gmail.com>
2022-11-24 20:01:23 +0400

    header.jsx

--- kasa/src/components/header/header.jsx
+++ kasa/src/components/header/header.jsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
 
-function Header()
-{
+import "./header.scss";
+
+import Logo from "../logo/logo";
+
+function Header() {
   return (
     <header className="header">
+      <Link 
+      className="header__logo" 
+      to="/">
+        <Logo 
+        fill="#FF6060" 
+        className="logo" />
+      </Link>
       <nav className="header__nav">
-        <Link className="nav-link" to="/">
+        <Link 
+        className="nav-link" 
+        to="/">
           Accueil
         </Link>
-        <Link className="nav-link" to="about">
+
+        <Link 
+        className="nav-link" 
+        to="about">
           A propos
         </Link>
       </nav>
