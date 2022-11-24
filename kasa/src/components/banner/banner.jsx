8a0c99b339e7850a51c9a59d529d1ca86c594c5b
Adele M <moutoussamy.adele@gmail.com>
2022-11-24 19:40:07 +0400

    Modifs banner.jsx

--- kasa/src/components/banner/banner.jsx
+++ kasa/src/components/banner/banner.jsx
@@ -1,16 +1,16 @@
-// J'importe le style.
-import "./banner.scss";
+import React from "react"
 
-function Banner({ title, picture })
-{
+import "./banner.scss"
+
+const Banner = ({ title, picture }) => {
   return (
     <div className="banner">
-        <img className="banner__img" src={picture} alt="Paysage, montagnes" />
-        <div className="banner__background">
-            <div className="banner__title">{title}</div>
-        </div>
+      <img className="banner__img" src={picture} alt="Paysage, montagnes" />
+      <div className="banner__background">
+        <div className="banner__title">{title}</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default Banner;
+export default Banner
