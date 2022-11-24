fc6efacb0b7ea05aff45c8711af1f7d4bd44f440
Adele M <moutoussamy.adele@gmail.com>
2022-11-24 19:59:03 +0400

    errors.jsx

---
+++ kasa/src/pages/errors/errors.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+import "./errors.scss";
+
+function Errors() {
+  return (
+    <div className="error">
+      <div className="error__number">404</div>
+      <div className="error__msg">
+        Oups! La page que vous demandez n'existe pas.
+      </div>
+      <Link
+        className="error__back-link"
+        to="/"
+        title="Retour page d'accueil"
+      >
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
+}
+
+export default Errors;
