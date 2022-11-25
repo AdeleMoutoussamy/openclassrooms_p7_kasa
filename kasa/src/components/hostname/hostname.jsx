fc6c0a329f3e112e04526821bdc86e1f5d3bcdcd
Adele M <moutoussamy.adele@gmail.com>
2022-11-25 12:12:34 +0400

    hostname.jsx

---
+++ kasa/src/components/hostname/hostname.jsx
@@ -0,0 +1,19 @@
+const HostName = ({ host }) => {
+  // Je split autour des espaces
+  const hostSplitName = host.name.split(" ")
+
+  const [name, lastname] = hostSplitName
+
+  return (
+    <div className="host">
+      <div className="host__name">
+        <p>{name}</p>
+        <p>{lastname}</p>
+      </div>
+
+      <img src={host.picture} alt="" className="host__picture" />
+    </div>
+  )
+}
+
+export default HostName
